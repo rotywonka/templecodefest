b10f46728d2c0aa88b253444963b42704a0ea985
Tommy Tan <tuf60598@temple.edu>
2018-04-15 01:03:29 -0400

    Eddystone Bash Boot

--- src/dht-firebase.py
+++ src/dht-firebase.py
@@ -1,6 +1,7 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+import serial, string, time
 import datetime
 from firebase import firebase
 #import Adafruit_DHT
@@ -14,6 +15,13 @@ from functools import partial
 #GPIO.cleanup()
 #GPIO.setwarnings(False)
 
+dict = {}
+output = " "
+ser = serial.Serial('/dev/ttyACM1', 9600, 8, 'N', 1, timeout=1)
+i = 0.0
+
+
+
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 #sensor = Adafruit_DHT.DHT11
@@ -24,7 +32,7 @@ from functools import partial
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = 1,2
+#humidity, temperature = 1,2
 # Adafruit_DHT.read_retry(sensor, pin)
 
 
@@ -33,15 +41,20 @@ firebase = firebase.FirebaseApplication('https://alarm-e979c.firebaseio.com/', N
 #firebase.put("/dht", "/humidity", "0.00")
 data = 'fuck leo'
 def update_firebase():
-	data = {"temp": temperature, "humidity": humidity}
-	firebase.post('/sensor/dht', data)
-	
+        
+    #data = {"Microphone1": }
+    while ser.in_waiting:
+        output = ser.readline()
+        dict = {'decibel': output}
+        print(output)
+        firebase.post('/sensor/', dict)
+#    firebase.post('', dict)	
 
 while True:
 		update_firebase()
 		
         #sleepTime = int(sleepTime)
-		sleep(5)
+		
 	
 
 
--- src/serialJSON.py
+++ src/serialJSON.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python
 import serial, string, time
-import json
-from ftplib import FTP
 
-ftp = FTP('https://files.000webhost.com/')
-ftp.login(user= 'tuf60598', passwd = 'plokijuh123')
-ftp.cwd('/json_filess/')
-
-dict = {}
-output = " "
-ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
-i = 0.0
-
-while True:
+def readSer():
+    dict = {}
+    output = " "
+    ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
+    i = 0.0
+    
     output = ser.readline()
-    dict[j] = {
-    'decibel': output
+    dict[i] = {
+        'decibel': output
     }
+        
     i = i + 0.00001
-    s = json.dumps(dict)
-    with open("/home/pi/templecodefest/src/json.txt", "w") as f:
-        f.write(s)
-
-    placefile()
-
-
-def placefile():
-
-    filename = "/home/pi/templecodefest/src/json.txt"
-    ftp.storbinary('STOR '+filename, open(filename, 'rb'))
-    ftp.quit()
+    return dict
